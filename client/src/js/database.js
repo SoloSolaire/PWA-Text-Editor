0c3a36984d01d23a15692fcc6ee7a447da8add66
SoloSolaire <128697724+SoloSolaire@users.noreply.github.com>
2023-08-21 21:13:48 -0700

    added logic to database.js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,10 +12,32 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  console.log('putting to db');
+  
+  const db = await openDB('jate', 1);
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+  const tx = db.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  const request = store.put({ id: 1, value: content });
+  const result = await request;
+  console.log(result + "; saved to db");
+};
+
+export const getDb = async () => {
+  console.error('getDb not implemented');
+const db = await openDB("jate" , 1);
+const tx = db.transaction("jate", "readonly");
+const store = tx.objectStore("jate");
+const request = store.get(1);
+const result = await request;
+
+if (result) {
+  console.log('data obtained :)', result.value);
+  return result.value;
+} else {
+  console.log('not found in db :(');
+}
+};
 
 initdb();
